b5a5ed0cc522c550dab648f0207b44f7194f89cd
T-zemmari <76817211+T-zemmari@users.noreply.github.com>
2021-02-13 19:28:15 +0100

    estructura en crecimiento ,colocados los divs de las imagenes y los titulos desde los javascript, son 16 de momento , tengo que refactorizar si no tendre un codigo de 1000 lineas

--- Proyecto 1/peliculas.js
+++ Proyecto 1/peliculas.js
@@ -1,9 +1,103 @@
 //Proyecto Replica Netflix
 
-const url = 'https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES';
+const url = 'https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&lang';
+
+ const divPeliculasDomElement1 =document.getElementById('id1');
+ const divPeliculasDomElement2 =document.getElementById('id2');
+ const divPeliculasDomElement3 =document.getElementById('id3');
+ const divPeliculasDomElement4 =document.getElementById('id4');
+ const divPeliculasDomElement5 =document.getElementById('id5');
+ const divPeliculasDomElement6 =document.getElementById('id6');
+ const divPeliculasDomElement7 =document.getElementById('id7');
+ const divPeliculasDomElement8 =document.getElementById('id8');
+ const divPeliculasDomElement9 =document.getElementById('id9');
+ const divPeliculasDomElement10 =document.getElementById('id10');
+ const divPeliculasDomElement11 =document.getElementById('id11');
+ const divPeliculasDomElement12 =document.getElementById('id12');
+ const divPeliculasDomElement13 =document.getElementById('id13');
+ const divPeliculasDomElement14 =document.getElementById('id14');
+ const divPeliculasDomElement15 =document.getElementById('id15');
+ const divPeliculasDomElement16 =document.getElementById('id16');
+
+
+   const mostarImagenes = () =>{
+    const obtenerJson = fetch(url).then(resultado => resultado.json());
+    const obtenerImagen = obtenerJson.then(data => data.results['0'].backdrop_path).then(console.log);
+   // const obtenerTitulo = obtenerJson.then(data => data.results['0'].title).then(console.log);
+   obtenerJson.then(data => {
+                       divPeliculasDomElement1.innerHTML =
+                       `<img src="" class="imgPeliculas" width="12em" height="12em" >
+                       <p class="titulo">${data.results['0'].title}</p>  `;
+                       divPeliculasDomElement2.innerHTML =
+                       `<img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo"> ${data.results['1'].title}</p>  `;
+                       divPeliculasDomElement3.innerHTML =            `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['2'].title}</p>  `;
+                       divPeliculasDomElement4.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['3'].title}</p>  `;
+                       divPeliculasDomElement5.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['4'].title}</p>  `;
+                       divPeliculasDomElement6.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['5'].title}</p>  `;
+                       divPeliculasDomElement7.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['6'].title}</p>  `;
+                       divPeliculasDomElement8.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo"> ${data.results['7'].title}</p>  `;
+                       divPeliculasDomElement9.innerHTML =
+                       `<img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo">${data.results['8'].title}</p>  `;
+                       divPeliculasDomElement10.innerHTML =
+                       `<img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo"> ${data.results['9'].title}</p>  `;
+                       divPeliculasDomElement11.innerHTML =            `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['10'].title}</p>  `;
+                       divPeliculasDomElement12.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['11'].title}</p>  `;
+                       divPeliculasDomElement13.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['12'].title}</p>  `;
+                       divPeliculasDomElement14.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['13'].title}</p>  `;
+                       divPeliculasDomElement15.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="8em" height="8em" >
+                       <p class="titulo" > ${data.results['14'].title}</p>  `;
+                       divPeliculasDomElement16.innerHTML = 
+                       `
+                       <img src="" class="imgPeliculas" width="12em" height="12em" >
+                       <p class="titulo"> ${data.results['15'].title}</p>  `;
 
 
 
+
+                       
+    }
+        )
+    
+    }
+
+     
+
+
+
+  /*const url = 'https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&lang'
   const getPelis = axios.get(url);
 
   let pelisArrayPromise = getPelis.then((response) => response.data.results );
@@ -19,8 +113,8 @@ const url = 'https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6
     const divPeliculasDomElement =document.getElementById('pelis');
     
 
-    miColeccionPelis.map((movie) => { 
-        const divTitulosDeLasPeliculas = document.createElement('div');
+      miColeccionPelis.map((movie) => { 
+       const divTitulosDeLasPeliculas = document.createElement('div');
         const divImagenesDeLasPeliculas = document.createElement('img');
         divImagenesDeLasPeliculas.setAttribute('class' , 'misImagenes');
         divImagenesDeLasPeliculas.setAttribute('src' , ' movie.backdrop_path')
@@ -37,3 +131,4 @@ const url = 'https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6
 }
       pelisArrayPromise.then(renderPelis);
 
+*/
